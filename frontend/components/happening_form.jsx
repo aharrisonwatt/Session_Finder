0373b260280a595fddd1e5e481f442cfa971487e
aharrisonwatt <aharrisonwatt@gmail.com>
2016-04-28 12:12:03 -0700

    HappeningForm skeleton

---
+++ frontend/components/happening_form.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+
+var HappeningForm = React.createClass({
+  getInitialState: function(){
+    return {
+      title: '',
+      body: '',
+      game: '',
+    };
+  },
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = HappeningForm;
