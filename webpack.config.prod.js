01dc3dad59999979c3e7e4c95a909911ad3f3c9c
aharrisonwatt <aharrisonwatt@gmail.com>
2016-05-16 15:26:12 -0700

    update webpack heroko build

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/session_finder.jsx",
+  output: {
+    path: './app/assets/javascripts',
+    filename: "bundle.js",
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
